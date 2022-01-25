e761b5796f3f40ba6610c1dac67070fd235d6438
Intihar12 <hussainintihar@gmail.com>
2022-01-24 22:10:08 -0800

    Initial message

---
+++ lib/home_scrren.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class myhomepage extends StatefulWidget {
+  const myhomepage({Key? key}) : super(key: key);
+
+  @override
+  _myhomepageState createState() => _myhomepageState();
+}
+
+class _myhomepageState extends State<myhomepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('hello'),
+      ),
+    );
+  }
+}
